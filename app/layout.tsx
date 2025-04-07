d843cdff37edfc9e35026fa86046879aa5ed9e88
zemedz <dimitris@dzmc-group.com>
2025-04-07 09:04:41 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
-export default function RootLayout({ children }) {
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+
   return (
     <html lang="en">
       <head />
